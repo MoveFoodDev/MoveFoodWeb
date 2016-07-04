05225f1c4bde79eb9e4ddc1ff954032c6d6ac467
Ben Hammond <Ben Hammond>
2016-07-04 14:37:06 -0700

    react boilerplate

--- config/secrets.example.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  firebase: {
-    dev: 'https://move-food-???-dev.firebaseio.com/',
-    test: 'https://move-food-???-test.firebaseio.com/',
-    prod: 'https://move-food-???.firebaseio.com/'
-  }
-};
